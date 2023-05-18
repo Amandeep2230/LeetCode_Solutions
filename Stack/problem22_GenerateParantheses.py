0ba2ff7d106c068adea585fbe6a4a3c449ae9cf6
Amandeep2230 <bhalla.aman99@gmail.com>
2023-05-18 17:57:51 -0400

    prob22

---
+++ Stack/problem22_GenerateParantheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack=[]
+        res=[]
+
+        def rec(openN, closedN):
+            if openN==closedN==n:
+                res.append("".join(stack))
+                return
+            
+            if openN<n:
+                stack.append("(")
+                rec(openN+1, closedN)
+                stack.pop()
+            
+            if closedN<openN:
+                stack.append(")")
+                rec(openN, closedN+1)
+                stack.pop()
+        rec(0,0)
+        return res
