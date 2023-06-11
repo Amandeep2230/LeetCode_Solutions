2c833c129aa0e86010fa77cadc4d7c952a0a3015
Amandeep2230 <bhalla.aman99@gmail.com>
2023-06-11 19:49:32 -0400

    prob108

---
+++ Misc/problem108_ConvertSortedArrayToBST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def con(l, r):
+            if l>r:
+                return None
+            m = (l+r)//2
+            root = TreeNode(nums[m])
+            root.left = con(l, m-1)
+            root.right = con(m+1,r)
+            return root
+        
+        return con(0, len(nums)-1)
